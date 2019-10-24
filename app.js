1dcb15cbf8d27522a76f3e0156ea68d7f3f2d6e4
Lonnie P <allon.paz@gmail.com>
2019-10-24 16:04:33 -0700

    figured out the issue with connecting to the database.  Pushing up incase I mess up and have to revert

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+
+const Event = require('./models/event');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql',
+    graphQlHttp({
+        schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+    type RootQuery {
+        events: [Event!]!
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+        rootValue: {
+            events: () => {
+                return events;
+            },
+            createEvent: args => {
+                const event = new Event({
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: new Date(args.eventInput.date)
+                });
+                return event
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        return { ...result._doc };
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
+            }
+        },
+        graphiql: true
+    })
+);
+
+// mongoose.connect(
+//     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uxaf6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+// ).then(() => {
+//     app.listen(3000);
+// })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+
+
+    const dbRoute = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uxaf6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(dbRoute, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(() => {
+    app.listen(3000);
+})
+    .catch(err => {
+        console.log(err);
+    });
+
+let DB = mongoose.connection;
+
+DB.once('open', () => console.log('connected to the database'));
+DB.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// mongoose.connect(
+//     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-60m3d.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+//         app.listen(3000);
+//     }).catch(err => {
+//         console.log(err);
+//     });
